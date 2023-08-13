16b3ea4017c421eee4c477f10c66d228517dd984
SteveH <stepan.hodik@gmail.com>
2023-08-13 14:14:06 +0200

    Add SOURCETABLE parsing, SocketCon for RTK corr

--- components/Header/Ntrip.js
+++ components/Header/Ntrip.js
@@ -1,105 +1,49 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Image, ScrollView, FlatList, TouchableOpacity, Buffer} from 'react-native';
+import React, {useState, useRef} from 'react';
+import {View, Text, TextInput, Button, ScrollView} from 'react-native';
+import Snackbar from 'react-native-snackbar';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import SelectDropdown from 'react-native-select-dropdown';
+import RowWithLabelAndValue from './RowWithLabelAndValue';
 import axios from 'axios';
+import {encode} from 'base-64';
+import TcpSocket from 'react-native-tcp-socket';
 
+class Mountpoint {
+  constructor(sourceTableString) {
+    const sourceTableData = sourceTableString.split(';');
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'RTK3-MSM',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa963',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e42972',
-    title: 'Third Item',
-  },
-
-];
-
-const Item = ({item, onPress, backgroundColor, textColor}) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}>
-    <Text style={[styles.title, {color: textColor}]}>{item.title}</Text>
-  </TouchableOpacity>
-);
-
+    this.id = sourceTableData[1];
+    this.name = sourceTableData[2];
+    this.format = sourceTableData[3];
+    this.carrier = sourceTableData[4];
+    this.navSystem = sourceTableData[6];
+    this.networkName = sourceTableData[7];
+    this.country = sourceTableData[8];
+    this.latitude = sourceTableData[9];
+    this.longitude = sourceTableData[10];
+    this.isVirtual = sourceTableData[11] === '1';
+    this.isNetworkSolution = sourceTableData[12] === '1';
+  }
+}
 
 const Ntrip = () => {
-  const [ntripIp, setNtripIp] = useState('http://195.245.209.181:2101');//http://euref-ip.net 195.245.209.181
-  const [ntripPort, setNtripPort] = useState('80'); //80 2101 
-  const [ntripUsername, setNtripUsername] = useState('cvutvyuka');
-  const [ntripPassword, setNtripPassword] = useState('k155dremejakokone');
-  const [mountpoint, setmountpoint] = useState('CPRG3-MSM'); //CPRG3-MSM
+  const [ntripSettings, setNtripSettings] = useState({
+    ntripIp: '195.245.209.181',
+    ntripPort: '2101',
+    ntripUsername: 'cvutvyuka',
+    ntripPassword: 'k155dremejakokone',
+    selectedMntp: null,
+    mountpoints: [],
+  });
 
-  const [selectedId, setSelectedId] = useState();
-  const [showFlatList, setShowFlatList] = useState(false); 
+  const mountpointSelectRef = useRef();
 
-  const [advice, setAdvice] = useState("");
-  const getRandomId = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return (Math.floor(Math.random() * 
-        (max - min + 1)) + min).toString();
-};
-
-  const getAdvice = () => {
-    axios
-        .get("http://api.adviceslip.com/advice/" + 
-            getRandomId(1, 200))
-        .then((response) => {
-            setAdvice(response.data.slip.advice);
-        });
-};
-
-
-  const renderItem = ({item}) => {
-    const backgroundColor = item.id === selectedId ? '#ccc' : '#ccc1';
-    const color = item.id === selectedId ? 'white' : 'black';
-
-    return (
-      <Item
-        item={item}
-        onPress={() => {
-        setSelectedId(item.id);
-        setShowFlatList(false); // Hide the FlatList after an item is selected}
-        }}
-        backgroundColor={backgroundColor}
-        textColor={color}
-      />
-    );
-  };
-
-
-
-  const handleNtripIpChange = (value) => {
-    setNtripIp(value);
-  };
-
-  const handleNtripPortChange = (value) => {
-    setNtripPort(value);
-  };
-
-  const handleNtripUsernameChange = (value) => {
-    setNtripUsername(value);
-  };
-
-  const handleNtripPasswordChange = (value) => {
-    setNtripPassword(value);
-  };
-
-
-  const handleNtripConnect = () => {
-
+  // Funkce pro aktualizaci nastavení
+  const updateNtripSettings = newSettings => {
+    setNtripSettings(prevSettings => ({
+      ...prevSettings,
+      ...newSettings,
+    }));
   };
 
   const handleMntpSelectChange = () => {
@@ -107,87 +51,180 @@ const Ntrip = () => {
     //setShowFlatList(true); // set to true when button is clicked
 
     console.log('Zkouším se připojit k Czepos');
-    
-    console.log(`${ntripIp}`);
-    axios.get(`${ntripIp}`)
-    .then((response) => {
-        console.log(response);
-    }).catch(
-      err=> console.log(err)
-      );
 
-
- /*
+    console.log(`http://${ntripSettings.ntripIp}:${ntripSettings.ntripPort}`);
     axios
-    .get("http://euref-ip.net")
-    .then((response) => {
-      console.log(response);
-    });
-*/
+      .get(`http://${ntripSettings.ntripIp}:${ntripSettings.ntripPort}`)
+      .then(response => {
+        const mountpoints = [];
+        const sourceData = response.data.split('\r\n');
+
+        for (let i = 0; i < sourceData.length; i++) {
+          if (sourceData[i].startsWith('STR')) {
+            mountpoints.push(new Mountpoint(sourceData[i]));
+          }
+        }
+
+        updateNtripSettings({
+          mountpoints: mountpoints,
+          selectedMntp: mountpoints[0],
+        });
+
+        mountpointSelectRef.current.selectIndex(0);
+      })
+      .catch(err => {
+        console.log(err.message);
+        updateNtripSettings({mountpoints: [], selectedMntp: null});
+        Snackbar.show({
+          text: `Chyba komunikace se serverem \r\nhttp://${ntripSettings.ntripIp}:${ntripSettings.ntripPort}`,
+          duration: Snackbar.LENGTH_SHORT,
+          textColor: 'red',
+          marginBottom: 5,
+        });
+      });
   };
 
-  const handleMntpSelectConnected = () => {
-    // Perform logic based on MNTP selection change
-    setShowFlatList(false); // set to true when button is clicked
-    setSelectedId(item.id);
+  //TODO : dodelat preposilani RTK korekci do GNSS - moznost vypnuti, restartu, ukladani mnozstvi stazenych dat
+  const onNtripConnect = () => {
+    console.log('Zkouším stahovat ntrip korekce');
+
+    const options = {
+      host: ntripSettings.ntripIp,
+      port: ntripSettings.ntripPort,
+    };
+
+    console.log(options);
+    // Create socket
+    const client = TcpSocket.createConnection(options, () => {
+      let connectionString =
+        'GET /' +
+        ntripSettings.selectedMntp.id +
+        ' HTTP/1.0\r\n' +
+        'Host: ' +
+        `http://${ntripSettings.ntripIp}:${ntripSettings.ntripPort}` +
+        '\r\n' +
+        'User-Agent: NTRIPClient for Arduino v1.0\r\n' +
+        'Authorization: ' +
+        `Basic ${utf8_to_b64(
+          ntripSettings.ntripUsername + ':' + ntripSettings.ntripPassword,
+        )}` +
+        '\r\n\r\n\r\n';
+
+      // Write on the socket
+      client.write(connectionString);
+
+      setTimeout(() => {
+        client.end();
+        console.log('Client byl ukončen : DEBUG!!');
+      }, 10000);
+    });
+
+    client.on('data', function (data) {
+      console.log('message was received', data);
+    });
+
+    client.on('error', function (error) {
+      console.log(error);
+    });
+
+    client.on('close', function () {
+      console.log('Connection closed!');
+    });
   };
 
   return (
-    <View style={styles.nastContainer}>
-
-      <Text style={styles.title}>Nastavení NTRIP připojení:</Text>
+    <ScrollView style={styles.nastContainer}>
+      <Text style={styles.title}>Nastavení NTRIP připojení</Text>
+      <Text>IP adresa NTRIP serveru:</Text>
       <TextInput
         style={styles.input}
         placeholder="Ip adresa serveru..."
-        value={ntripIp}
-        onChangeText={handleNtripIpChange}
+        value={ntripSettings.ntripIp}
+        onChangeText={text => updateNtripSettings({ntripIp: text})}
       />
+      <Text>Port NTRIP serveru:</Text>
       <TextInput
         style={styles.input}
         placeholder="port..."
-        value={ntripPort}
-        onChangeText={handleNtripPortChange}
+        value={ntripSettings.ntripPort}
+        onChangeText={text => updateNtripSettings({ntripPort: text})}
       />
-      <Button title="MountPointy" style={styles.button} onPress={handleMntpSelectChange} />
-      <View style={styles.hrLine} />
-      <View style={{ height: 150 }}>
-      {showFlatList && ( // conditional rendering based on the new piece of state
-        <FlatList
-          data={DATA}
-          renderItem={renderItem}
-          keyExtractor={item => item.id}
-          extraData={selectedId}
+      <Text>Vyber mountpoint:</Text>
+      <View style={styles.selectDropdown}>
+        <SelectDropdown
+          style={styles.selectDropdown}
+          ref={mountpointSelectRef}
+          data={ntripSettings.mountpoints.map(mntp => mntp.name)}
+          disabled={ntripSettings.mountpoints.length === 0}
+          defaultValueByIndex={0}
+          defaultButtonText="žádná data"
+          buttonStyle={styles.dropdownBtnStyle}
+          onSelect={(_, index) => {
+            updateNtripSettings({
+              selectedMntp: ntripSettings.mountpoints[index],
+            });
+          }}
+          renderDropdownIcon={isOpened => {
+            return (
+              <FontAwesome5
+                name={isOpened ? 'chevron-up' : 'chevron-down'}
+                color={'#444'}
+                size={18}
+              />
+            );
+          }}
+          dropdownIconPosition={'right'}
         />
-      )}
       </View>
-      <Text style={styles.title}>Připojení k MountPointu:</Text>
+      <Button
+        title="Vyhledej MountPointy"
+        style={styles.button}
+        onPress={handleMntpSelectChange}
+      />
+      <View style={styles.hrLine} />
+      <Text style={styles.title}>Připojení k NTRIP serveru:</Text>
       <TextInput
         style={styles.input}
         placeholder="Uživatelské jméno"
-        value={ntripUsername}
-        onChangeText={handleNtripUsernameChange}
+        value={ntripSettings.ntripUsername}
+        onChangeText={text => updateNtripSettings({ntripUsername: text})}
       />
       <TextInput
         style={styles.input}
         placeholder="Heslo"
-        value={ntripPassword}
-        onChangeText={handleNtripPasswordChange}
+        value={ntripSettings.ntripPassword}
+        onChangeText={text => updateNtripSettings({ntripPassword: text})}
         secureTextEntry
       />
-      <Button title="Připoj" id="NTRIPpripoj" style={styles.button} onPress={handleNtripConnect} />
+      <Button
+        title="Připoj se k NTRIP serveru"
+        id="NTRIPpripoj"
+        style={styles.button}
+        onPress={onNtripConnect}
+      />
       <View style={styles.hrLine} />
-      <Text style={styles.title}>Podrobnosti mountpointu</Text>
-
-      <View id="mntpTable" style={styles.mntpTable} >
-      <Text>{selectedId}</Text>
-      </View>
-      <View style={styles.container}>
-            <Text style={styles.advice}>{advice}</Text>
-            <Button title="Get Advice" 
-                onPress={getAdvice} color="green" />
-      </View>
-      
-    </View>
+      {ntripSettings.selectedMntp !== null && (
+        <View style={styles.mountpointInfo}>
+          <Text style={styles.title}>Podrobnosti mountpointu</Text>
+          <RowWithLabelAndValue
+            label="Název"
+            value={ntripSettings.selectedMntp.id}
+          />
+          <RowWithLabelAndValue
+            label="Formát dat"
+            value={ntripSettings.selectedMntp.format}
+          />
+          <RowWithLabelAndValue
+            label="Navigační sytémy"
+            value={ntripSettings.selectedMntp.navSystem}
+          />
+          <RowWithLabelAndValue
+            label="Je virtuální"
+            value={ntripSettings.selectedMntp.isVirtual ? 'Ano' : 'Ne'}
+          />
+        </View>
+      )}
+    </ScrollView>
   );
 };
 
@@ -197,16 +234,6 @@ const styles = {
   nastContainer: {
     paddingHorizontal: 16,
     paddingVertical: 8,
-  },
-  appVersion: {
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
-  appVersionNumber: {
-    fontWeight: 'normal',
-  },
-  appRelease: {
-    fontWeight: 'normal',
   },
   hrLine: {
     borderBottomColor: '#ccc',
@@ -222,24 +249,6 @@ const styles = {
     alignItems: 'center',
     marginBottom: 8,
   },
-  refreshButton: {
-    marginRight: 8,
-    padding: 8,
-  },
-  refreshIcon: {
-    width: 24,
-    height: 24,
-  },
-  selectContainer: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
-    marginRight: 8,
-  },
-  select: {
-    height: 40,
-  },
   input: {
     borderWidth: 1,
     borderColor: '#ccc',
@@ -247,24 +256,27 @@ const styles = {
     marginBottom: 8,
     padding: 8,
   },
+  dropdownBtnStyle: {
+    width: '100%',
+    height: 35,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    marginTop: 8,
+    marginBottom: 8,
+  },
+  dropdownBtnTxtStyle: {color: '#444', textAlign: 'left'},
   button: {
     marginBottom: 8,
   },
-  mntpTable: {
-    marginBottom: 16,
-  },
-  item: {
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+  mountpointInfo: {
+    paddingBottom: 25,
   },
 };
 
 // Helper functions
 function utf8_to_b64(str) {
-  return Buffer.from(str, 'utf-8').toString('base64');
-}
-
-function b64_to_utf8(str) {
-  return Buffer.from(str, 'base64').toString('utf-8');
+  const bytes = encode(str);
+  return bytes;
 }
