a601b6499a1d0f0cf3ee2f95e625dce219aff285
zahrada <zahradnik.geodet.dz@gmail.com>
2023-11-15 17:03:00 +0100

    fix

--- components/Header/Placing.js
+++ components/Header/Placing.js
@@ -7,25 +7,55 @@
 // Zobrazit uhel a delku k bodu
 // Ukazat smerovku
 
-import React, { useState, useEffect, useContext } from 'react';
-import { SafeAreaView, View, Text, Button, FlatList, TouchableOpacity, ScrollView, PermissionsAndroid, Platform, TextInput, Alert, Switch } from 'react-native';
+import React, {useState, useEffect, useContext} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  ScrollView,
+  PermissionsAndroid,
+  Platform,
+  TextInput,
+  Alert,
+  Switch,
+} from 'react-native';
 import Snackbar from 'react-native-snackbar';
-import { styles } from '../Styles/styles';
+import {styles} from '../Styles/styles';
 import IconMaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import { etrs2jtsk, jtsk2etrs } from '../Calculations/transformation'
+import {etrs2jtsk, jtsk2etrs} from '../Calculations/transformation';
 import GPS from 'gps';
-import { DataContext } from '../Functions/DataContext';
-import { FlatListPoint } from './ProjectComponents/FlatListPoint';
+import {DataContext} from '../Functions/DataContext';
 
+
+// Funkce pro nacteni dat zakazky
+const ItemPoint = ({item, onPress, backgroundColor, textColor, textColor1}) => (
+  <ScrollView
+    horizontal
+    contentContainerStyle={styles.scrollViewContent}
+    showsHorizontalScrollIndicator={false}>
+    <Text style={[styles.title, {color: textColor1}]}>{item.title} </Text>
+    <Text style={[styles.title, {color: textColor1}]}>B </Text>
+    <Text style={[styles.title, {color: textColor}]}>{item.b} </Text>
+    <Text style={[styles.title, {color: textColor1}]}>L </Text>
+    <Text style={[styles.title, {color: textColor}]}>{item.l} </Text>
+    <Text style={[styles.title, {color: textColor1}]}>H </Text>
+    <Text style={[styles.title, {color: textColor}]}>{item.h} </Text>
+  </ScrollView>
+);
 
 export const Placing = () => {
-
-  const { data, updateData } = useContext(DataContext);
-  const gps = new GPS;
+  const {data, updateData} = useContext(DataContext);
+  const gps = new GPS();
   var NMEA = null;
-  var dist;
+  const [dist, setDist] = useState(0);
   var heading;
-  const [points, setPoints] = useState(data.projects[data.projectSettings.projectId].points);
+  const [points, setPoints] = useState(
+    data.projects[data.projectSettings.projectId].points,
+  );
+  const [point, setPoint] = useState(points[0]);
 
   // console.log(data.projects[0].title);
 
@@ -36,24 +66,8 @@ export const Placing = () => {
 
   // Call the update routine directly with a NMEA sentence, which would
   // come from the serial port or stream-reader normally
-  gps.update("$GPGGA,224900.000,5032.3762,N,01403.5393,E,1,04,7.8,498.6,M,48.0,M,,0000*5E");
-
-  // Funkce pro nacteni dat zakazky
-  const ItemPoint = ({ item, onPress, backgroundColor, textColor, textColor1 }) => (
-    <ScrollView
-      horizontal
-      contentContainerStyle={styles.scrollViewContent}
-      showsHorizontalScrollIndicator={false}>
-      <Text style={[styles.title, { color: textColor1 }]}>{item.title} </Text>
-      <Text style={[styles.title, { color: textColor1 }]}>B </Text>
-      <Text style={[styles.title, { color: textColor }]}>{item.b} </Text>
-      <Text style={[styles.title, { color: textColor1 }]}>L </Text>
-      <Text style={[styles.title, { color: textColor }]}>{item.l} </Text>
-      <Text style={[styles.title, { color: textColor1 }]}>H </Text>
-      <Text style={[styles.title, { color: textColor }]}>{item.h} </Text>
-    </ScrollView>
-
-
+  gps.update(
+    '$GPGGA,224900.000,5032.3762,N,01403.5393,E,1,04,7.8,498.6,M,48.0,M,,0000*5E',
   );
 
   // Kopie ukladani souradnic bodu z importu
@@ -62,42 +76,21 @@ export const Placing = () => {
     const x = parseFloat(ItemPoint.item.b);
     const y = parseFloat(ItemPoint.item.l);
     const z = parseFloat(ItemPoint.item.h);
-    console.log(data.projects[data.projectSettings.projectId].points)
+    console.log(data.projects[data.projectSettings.projectId].points);
   };
 
-  const calculate = (dist, heading, gps, points) => {
-    dist = GPS.Distance(gps.lat, gps.lon, points[0].b, points[0].l) / 1000
-    heading = GPS.Heading(gps.lat, gps.lon, points[0].b, points[0].l)
-    console.log(points[0].b)
-    console.log(gps)
-    console.log(dist)
-    console.log(heading)
+  const calculate = (dist, heading, gps) => {
+    setDist(GPS.Distance(gps.lat, gps.lon, point.b, point.l) / 1000);
+    heading = GPS.Heading(gps.lat, gps.lon, point.b, point.l);
+    console.log(gps);
+    console.log(dist);
+    console.log(heading);
   };
 
-  const [projectSettings, setProjectSettings] = useState(data.projectSettings);
-  const updateProjectSettings = newSettings => {
-    setProjectSettings(prevSettings => ({
-      ...prevSettings,
-      ...newSettings,
-    }));
-  };
-
-
-  useEffect(() => {
-    Snackbar.show({
-      text: 'Tato funkce je ve vývoji',
-      duration: Snackbar.LENGTH_SHORT,
-      textColor: 'red',
-      marginBottom: 5,
-    });
-
-  }, []);
-
-  const renderItemPoint = ({ item }) => {
+  const renderItemPoint = ({item}) => {
     return (
       <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          onPress={console.log('funguju')}>
+        <TouchableOpacity onPress={setPoint(item)}>
           <ItemPoint item={item} textColor={'gray'} textColor1={'white'} />
         </TouchableOpacity>
       </View>
@@ -106,30 +99,18 @@ export const Placing = () => {
 
   return (
     <View style={styles.container}>
-      <Text
-        style={styles.title}>Vytyčení</Text>
-      <Text
-        style={styles.text}>Zvol bod z aktivni zakazky:
-      </Text>
+      <Text style={styles.title}>Vytyčení</Text>
+      <Text style={styles.text}>Zvol bod z aktivni zakazky:</Text>
       <FlatList
-        data={data.projects[data.projectSettings.projectId].points}
+        data={points}
         renderItem={renderItemPoint}
         keyExtractor={item => item.title}
       />
-      <Button
-        title="Vytyčuj"
-        onPress={calculate(dist, heading, gps, points)}
-      />
+      <Button title="Vytyčuj" onPress={() => {calculate(dist, heading, gps, points)}} />
       <View style={styles.buttonContainer}>
-        <Text
-          style={styles.title}>Vzdalenost: {dist}</Text>
-
-
+        <Text style={styles.title}>Vzdalenost: {dist}</Text>
       </View>
     </View>
-
   );
 };
-;
-
 export default Placing;
