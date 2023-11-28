71cd7e9e7559d52b6bd0c1876fe66f98058b0738
František Gurecký <gurecfra@cvut.cz>
2023-11-28 21:42:41 +0100

    FlatListPoint info button
    FlatListPoint info button now shows coordinates, added style for Modal view, didn't yet add JTSK coordinates to point info.

--- components/Header/ProjectComponents/FlatListPoint.js
+++ components/Header/ProjectComponents/FlatListPoint.js
@@ -1,22 +1,25 @@
-import React, { useContext } from 'react';
-import { FlatList, View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { FlatList, View, Text, TouchableOpacity, ScrollView, Modal } from 'react-native';
 import IconMaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { DataContext } from '../../Functions/DataContext';
 import { styles } from '../../Styles/styles';
 
-const ItemPoint = ({ item, onPress, backgroundColor, textColor, textColor1 }) => (
+const ItemID = ({ item, textColor }) => (
   <ScrollView
     horizontal
     contentContainerStyle={styles.scrollViewContent}
     showsHorizontalScrollIndicator={false}>
     <Text style={[styles.title, { color: textColor }]}>{item.title} </Text>
-    <Text style={[styles.title, { color: textColor }]}>B </Text>
-    <Text style={[styles.title, { color: textColor }]}>{item.b} </Text>
-    <Text style={[styles.title, { color: textColor }]}>L </Text>
-    <Text style={[styles.title, { color: textColor }]}>{item.l} </Text>
-    <Text style={[styles.title, { color: textColor }]}>H </Text>
-    <Text style={[styles.title, { color: textColor }]}>{item.h} </Text>
   </ScrollView>
+);
+
+const ItemInfo = ({ item, textColor }) => (
+  <View>
+    <Text style={[styles.title, { color: textColor }]}>Bod: {item.title}</Text>
+    <Text style={[styles.title, { color: textColor }]}>B: {item.b}</Text>
+    <Text style={[styles.title, { color: textColor }]}>L: {item.l}</Text>
+    <Text style={[styles.title, { color: textColor }]}>H: {item.h}</Text>
+  </View>
 );
 
 export default FlatListPoint = ({
@@ -25,11 +28,20 @@ export default FlatListPoint = ({
   placing,
 }) => {
   const { data, updateData } = useContext(DataContext);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedPoint, setSelectedPoint] = useState(null);
 
-  const renderItemPoint = ({ item, index }) => {
+  const renderItemID = ({ item, index }) => {
     return (
       <View style={styles.buttonContainer}>
-        <ItemPoint item={item} textColor={'gray'} textColor1={'white'} />
+        <ItemID item={item} textColor={'gray'} />
+        <TouchableOpacity
+          onPress={() => {
+            setSelectedPoint(item);
+            setModalVisible(true);
+          }}>
+          <IconMaterialIcons name="info" size={24} color="black" />
+        </TouchableOpacity>
         {!placing &&
           <TouchableOpacity
             onPress={() => {
@@ -71,10 +83,30 @@ export default FlatListPoint = ({
     <View style={{ height: 120 }}>
       <FlatList
         data={data.projects[projectSettings.projectId].points}
-        renderItem={renderItemPoint}
+        renderItem={renderItemID}
         keyExtractor={item => item.title}
       />
       <View style={styles.hrLine} />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}>
+        <View style={{ marginTop: 22 }}>
+          <View style={styles.modalView}>
+            <ItemInfo item={selectedPoint} textColor={'black'} />
+            <TouchableOpacity
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}>
+              <Text>Zavřít okno</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
+
   );
 };
