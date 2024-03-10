001392109b7086b039251c50b58b1aff3a09fac8
František Gurecký <gurecfra@cvut.cz>
2024-03-10 17:58:01 +0100

    initialRegion changes, markers shown
    initialRegion should be the center of all points, but is set after rendering, so it doesn't work as indended, needs a small fix. Markers are shown from projects, will add onPress function to show more properties in the near future.

--- components/Header/Map.js
+++ components/Header/Map.js
@@ -1,11 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { SafeAreaView, View, Text, Button, PermissionsAndroid, Platform } from 'react-native';
 import Snackbar from 'react-native-snackbar';
 import { styles } from '../Styles/styles';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { debounce } from 'lodash';
+import configurationData from '../configurationData';
+import { DataContext } from '../Functions/DataContext';
 
 
-export const Map = () => {
+export const Map = ({}) => {
+  const { data } = useContext(DataContext);
+  const point = data.projects[data.projectSettings.projectId].points
+  const [initialRegion, setInitialRegion] = useState(null);
+  const [region, setRegion] = useState(initialRegion);
+  const debouncedSetRegion = debounce(setRegion, 100);
+
   useEffect(() => {
     Snackbar.show({
       text: 'Tato funkce je ve vývoji',
@@ -15,20 +24,58 @@ export const Map = () => {
     });
   }, []);
 
+  useEffect(() => {
+    if (point.length > 0 && !initialRegion) {
+      const latitudes = point.map((point) => point.b);
+      const longitudes = point.map((point) => point.l);
+
+      const minLat = Math.min(...latitudes);
+      const maxLat = Math.max(...latitudes);
+      const minLng = Math.min(...longitudes);
+      const maxLng = Math.max(...longitudes);
+
+      const deltaLat = Math.abs(maxLat - minLat);
+      const deltaLng = Math.abs(maxLng - minLng);
+
+      setInitialRegion({
+        latitude: (maxLat + minLat) / 2,
+        longitude: (maxLng + minLng) / 2,
+        latitudeDelta: deltaLat * 1.5, // Add some padding to the delta
+        longitudeDelta: deltaLng * 1.5, // Add some padding to the delta
+      });
+    }
+  }, [point]);
+
+  useEffect(() => {   /// SMAZAT
+    console.log('InitialRegion: ', initialRegion);
+  }, [initialRegion]);
+
+  const onRegionChange = (newRegion) => {
+    debouncedSetRegion(newRegion);
+  };
+
+  useEffect(() => {
+    return () => {
+      debouncedSetRegion.cancel();
+    };
+  }, []);
+
   return (
     <View style={styles.mapContainer}>
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
-        initialRegion={{
-          latitude: 50.1042375,
-          longitude: 14.3883522,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.015,
-        }}>
-        <Marker
-          coordinate={{ latitude: 50.1042375, longitude: 14.3883522 }}
-        />
+        region={region}
+        onRegionChange={onRegionChange}
+      >
+        {point.map((point, index) => (
+          <Marker
+            key={index}
+            coordinate={{ latitude: point.b, longitude: point.l }}
+            title={point.title}
+            calloutEnabled={true}
+          />
+        ))}
       </MapView>
     </View>
   );
