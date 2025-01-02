0bf9412149a99e469bd754890bed19f7e6e1d69f
František Gurecký <gurecfra@cvut.cz>
2025-01-02 19:41:22 +0100

    fix: App renaming
    Project initilaized as "AwesomeProject", variables replaced with "K155GNSSApp"

--- android/app/src/main/java/com/k155gnssapp/MainApplication.java
+++ android/app/src/main/java/com/k155gnssapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.k155gnssapp;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/AwesomeProjectTests/AwesomeProjectTests.m
+++ ios/AwesomeProjectTests/AwesomeProjectTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface K155GNSSAppTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation K155GNSSAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
